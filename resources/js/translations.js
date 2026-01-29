4e5cf3bc859d312cb972b08ea51e6abaf68bb42e
יובל משיח <myyuval555@gmail.com>
2026-01-29 11:51:57 +0200

    Upgrade home page

--- resources/js/translations.js
+++ resources/js/translations.js
@@ -16,7 +16,8 @@ const dictionary = {
         hero_student: "סטודנט שנה ג' למדעי המחשב במרכז האקדמי לב",
         hero_location: "ישראל",
         btn_seemore: "מידע נוסף",
-        btn_myskills: "לכל הכישורים שלי",
+        btn_projects: "הפרויקטים שלי",
+        skill_view_all: "לכל הכישורים",
 
         // --- Categories ---
         cat_languages: "שפות תכנות",
@@ -98,7 +99,8 @@ const dictionary = {
         hero_student: "3rd Year BA student in Computer Science at JCT",
         hero_location: "Israel",
         btn_seemore: "See More",
-        btn_myskills: "Full Skill Set",
+        btn_projects: "My Projects",
+        skill_view_all: "View All",
 
         // --- Categories ---
         cat_languages: "Languages",
